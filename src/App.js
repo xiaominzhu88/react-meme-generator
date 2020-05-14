8f409371b1248e52088dfd6de6d3a3eb1d953ca9
xiaomin zhu <xiaomin.zhu88@gmail.com>
2020-05-14 19:09:55 +0200

    Add comment to make the step clear

--- src/App.js
+++ src/App.js
@@ -8,19 +8,24 @@ function App() {
   const [imgUrl, setImgUrl] = useState(
     'https://memegen.link/tried/at_least/you_tried.jpg?preview=true&watermark=none',
   );
-// fetch site and get data values
+
+  // fetch url and get data values
   useEffect(() => {
     fetch('https://memegen.link/api/templates/', {
       headers: { 'Content-Type': 'application/json' },
     })
       .then((res) => res.json())
       .then((data) => {
+        // Object.values shows all the links, KEYS should show all the names
         const imgData = Object.values(data);
+
+        // Here undate the dropdown list urls into NEW selectUrl, it is an Array, in return map over this Array to get each of url
         setSelectUrl(imgData);
       })
       .catch((e) => console.log(e));
   }, []);
 
+  // get input possible
   function inputText(event) {
     setTextFirstInput(event.target.value);
   }
@@ -28,7 +33,7 @@ function App() {
   function inputText2(event) {
     setTextSecondInput(event.target.value);
   }
-  
+  // update imgUrl with input value, get the hole Path, used  with button event
   function createURL(e) {
     setImgUrl(imgUrl + '/' + textFirstInput + '/' + textSecondInput + '.jpg');
   }
@@ -38,11 +43,15 @@ function App() {
       <h1>React-meme-Generator</h1>
 
       <br />
+
+      {/* Create Dropdown List to show all the fetched data-links */}
       <select
         onChange={(e) => {
           setImgUrl(e.target.value);
         }}
       >
+        {/* Show all the Dropdown List Values */}
+
         {selectUrl.map((name, i) => {
           return (
             <option key={i} value={name.replace('api/templates/', '')}>
@@ -55,11 +64,7 @@ function App() {
       <br />
 
       <input value={textFirstInput} onChange={inputText} />
-      <input
-        value={textSecondInput}
-        onChange={inputText2}
-      
-      />
+      <input value={textSecondInput} onChange={inputText2} />
 
       <br />
       <button onClick={createURL}>Go for text image!</button>
@@ -72,7 +77,10 @@ function App() {
       >
         Clear
       </button>
-      <p>{imgUrl}</p>
+
+      {/* Good idea : check the current imgUrl */}
+
+      {/*<p>{imgUrl}</p>*/}
       <br />
       <img src={imgUrl + '.jpg'} alt=" " />
     </div>
