3dd9924073066d480f4d27cc8ce2ae099feeddcf
xiaomin zhu <xiaomin.zhu88@gmail.com>
2020-05-14 17:44:29 +0200

    Add input Value and dropdown list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [textFirstInput, setTextFirstInput] = useState('');
+  const [textSecondInput, setTextSecondInput] = useState('');
+  const [selectUrl, setSelectUrl] = useState([]);
+  const [imgUrl, setImgUrl] = useState(
+    'https://memegen.link/tried/at_least/you_tried.jpg?preview=true&watermark=none',
+  );
+
+  useEffect(() => {
+    fetch('https://memegen.link/api/templates/', {
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const imgData = Object.values(data);
+        setSelectUrl(imgData);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  function inputText(event) {
+    setTextFirstInput(event.target.value);
+  }
+
+  function inputText2(event) {
+    setTextSecondInput(event.target.value);
+  }
+
+  function createURL() {
+    const path =
+      'https://memegen.link/tried/' +
+      textFirstInput +
+      '/' +
+      textSecondInput +
+      '.jpg?preview=true&watermark=none';
+
+    setImgUrl(path);
+  }
+
+  //function onChangeSelect(e){
+  //  setSelectUrl(e.target.value);
+  //}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React-meme-Generator</h1>
+
+      <br />
+      <select
+        onChange={(e) => {
+          setImgUrl(e.target.value);
+        }}
+      >
+        {selectUrl.map((name, i) => {
+          return (
+            <option key={i} value={name.replace('api/templates/', '')}>
+              {name.replace('api/templates/', '')}
+            </option>
+          );
+        })}
+      </select>
+
+      <br />
+
+      <input value={textFirstInput} onChange={inputText} placeholder="/path1" />
+      <input
+        value={textSecondInput}
+        onChange={inputText2}
+        placeholder="/path2"
+      />
+
+      <br />
+      <button onClick={createURL}>Go for text image!</button>
+      <button
+        onClick={() => {
+          setTextFirstInput('');
+          setTextSecondInput('');
+        }}
+      >
+        Clear
+      </button>
+      <p>{imgUrl}</p>
+      <br />
+      <img src={imgUrl + '.jpg'} alt=" " />
     </div>
   );
 }
